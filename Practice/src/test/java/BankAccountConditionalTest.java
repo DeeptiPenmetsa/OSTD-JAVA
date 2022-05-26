3a19f3152a69eda1d6e2bb0378c768c3ef9f75b8
DeeptiPriya Penmetsa <deeptipriya_penmetsa@epam.com>
2022-05-26 12:43:14 +0530

    junit

---
+++ Practice/src/test/java/BankAccountConditionalTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+public class BankAccountConditionalTest {
+
+    @Test
+    @EnabledOnOs({OS.MAC})
+    public void testMac(){
+
+    }
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
+    public void testWindows(){
+
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_8})
+    public void testJRE(){
+
+    }
+
+    @Test
+    @DisabledOnJre({JRE.JAVA_8})
+    public void testNoJRE8(){
+
+    }
+}
